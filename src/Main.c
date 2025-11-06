aaa9f123b513571a4b28e9fccb6e7a4dea38e7c9
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-06 18:17:45 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,19 +1,25 @@
-#include "C:/Wichtig/System/Static/Container/MVector.h"
-#include "C:/Wichtig/System/Static/Library/w->ElapsedTime.h"
-#include <Windows.h>
+#if defined(__linux__)
+#include "/home/codeleaded/System/Static/Container/MVector.h"
+#elif defined(_WINE)
+#include "/home/codeleaded/System/Static/Container/MVector.h"
+#elif defined(_WIN32)
+#include "F:/home/codeleaded/System/Static/Container/MVector.h"
+#elif defined(__APPLE__)
+#error "Apple not supported!"
+#else
+#error "Platform not supported!"
+#endif
+
 
 int main(){
     MVector v = MVector_New(sizeof(char));
 
     for(int i = 0;i<100000000;i++){
-        if(v.size % 10000 == 0) printf("End: XSize: %llu, Size: %llu, Size: %llu\n",v.ExpandSize,v.size,v.SIZE);
+        if(v.size % 10000 == 0) printf("End: XSize: %lu, Size: %lu, Size: %lu\n",v.ExpandSize,v.size,v.SIZE);
         MVector_Push(&v,(char[]){'0'});
         //printf("%c\n",c);
-        
     }
-    printf("End: XSize: %llu, Size: %llu, Size: %llu\n",v.ExpandSize,v.size,v.SIZE);
-
-    Sleep(1000);
+    printf("End: XSize: %lu, Size: %lu, Size: %lu\n",v.ExpandSize,v.size,v.SIZE);
 
     MVector_Free(&v);
 
